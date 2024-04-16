9ee49a13b70b5ca2dcfa8477526d79cb4697c66a
Eli Lawrence <146491903+eli-lawrence@users.noreply.github.com>
2024-04-16 12:38:28 -0700

    Update main.cpp
    Removed entire air hockey function and prototype created in main.

--- main.cpp
+++ main.cpp
@@ -1,77 +1,9 @@
 #include "SFML_header.hpp"
 
-int hockey(void);
 int shooter(void);
 
 int main(void) {
     shooter();
-}
-
-int hockey(void) {
-
-    //--------------------------INITIALIZATION-----------------------------------
-    int radius = 80;
-    float speed = radius / 5;
-
-    sf::RenderWindow window(sf::VideoMode(WINDOW_W, WINDOW_H), "SFML works!"); //creates window called window and opens it
-    //window is opened in video mode ??? //400, 400 specifies height and width, string specifies window title
-
-    sf::CircleShape mallet_1(radius); //creates circle
-    mallet_1.setPosition(sf::Vector2f(200, 10));
-    mallet_1.setFillColor(sf::Color::Blue); //sets circle color to green
-    center_origin(mallet_1);
-
-    sf::CircleShape mallet_2(radius); //creates circle
-    mallet_2.setPosition(sf::Vector2f(200, 390));
-    mallet_2.setFillColor(sf::Color::Green); //sets circle color to green
-    center_origin(mallet_2);
-
-    sf::CircleShape puck(radius); //creates circle
-    puck.setPosition(sf::Vector2f(WINDOW_W/2, WINDOW_H/2));
-    puck.setFillColor(sf::Color::Red); //sets circle color to green
-    center_origin(puck);
-
-    sf::RectangleShape half_court(sf::Vector2f(WINDOW_W, 2));
-    half_court.setPosition(sf::Vector2f(WINDOW_W / 2, WINDOW_H / 2));
-    center_origin(half_court);
-
-       //program loop
-    while (window.isOpen()) {
-
-        //-----------------------------------------UPDATE--------------------------------
-
-        //event loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-                window.close();
-            }
-        }
-
-        //movement
-        key_move(mallet_1, speed, "aswd", sf::FloatRect(0, 0, WINDOW_W, WINDOW_H / 2));
-        mouse_move(mallet_2, window, sf::FloatRect(0, WINDOW_H / 2, WINDOW_W, WINDOW_H / 2));
-        //key_move(mallet_2, speed, "arrow", sf::FloatRect(0, WINDOW_H / 2, WINDOW_W, WINDOW_H / 2));
-        puck_slide(puck, mallet_1, mallet_2);
-
-        //------------------------------------------DRAW------------------------
-
-        //clears screen with black pixels
-        window.clear();
-
-        window.draw(half_court);
-        window.draw(mallet_1);
-        window.draw(mallet_2);
-        window.draw(puck);
-        window.display();
-
-    }
-
-    return 0;
-
 }
 
 int shooter(void) {
