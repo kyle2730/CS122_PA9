1f4e1ae88464a26b3ab3ff9ed96ab6c79ea8d863
Eli Lawrence <146491903+eli-lawrence@users.noreply.github.com>
2024-04-19 13:22:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
 #include "SFML_header.hpp"
 #include "item.hpp";
 
-int shooter(void);
-
 int main(void) 
 {
-   bool selection = menu();
-
-   if (selection == true)
-   {
-       shooter();
-   }
-   else
-   {
-       return 0;
-   }
-}
-
-int shooter(void) {
-
+    if (!menu()) return 0;
+   
     //--------------------------INITIALIZATION-----------------------------------
     float circ_radius = 10, speed = 8;
 
     srand(time(NULL));
-
-    if (!menu()) return 0;
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_W, WINDOW_H), "Game Name"); //creates window called window and opens it
     //window is opened in video mode ??? //400, 400 specifies height and width, string specifies window title
@@ -113,33 +97,3 @@ int shooter(void) {
     return 0;
 
 }
-
-
-//game ideas
-/*
-
-1. Killing Tux the Penguin (tap a specific number of times in 10 seconds to kill tux)
-    - alternative gameplay ideas: 
-        - chase Tux around some board/field
-        - hit specific keys on keyboard fast, instead of tapping same key fast
-
-2. Pac-Man (ghost chases character controlled by user through maze until character collects all orbs)
-    - Game Features:
-        - 1 ghost (moves master as levels increase)
-        - smaller map
-
-3. Air Hockey or Tennis (two player game between user and A.I or user and another user through docks)
- 
-4. Shooter Game (user-controlled player fires projectiles at randomly spawned enemies)
-    - Game Features:
-        - player runs through map, past enemies that shoot back (player must dodge bullets)
-        - player gets health, once health = 0, player dies
-        - multiple rounds with more/faster/stronger/bigger/scarier enemies
-        - helpful items fly across screen for player to collect
-            - items can add lives, make user faster, make user smaller, etc.
-            - can add negative items as well
-    - Alternative features
-        - obstacles on map
-
-CS122_PA9/ - use to load images
-*/
